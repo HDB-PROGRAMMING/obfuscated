90153e8ff80178dc77ea9023f83641bd76c6ea87
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-20 17:39:26 +0000

    Create obfuscated D hello world

---
+++ D/hello.d
@@ -0,0 +1,15 @@
+//m****x
+int _0xAA(char _P){
+    static int _0x00=0x01;
+    if (_0x00) {int _0xAD=0x00;}
+else _0x00=0x00;
+return _0x00;}
+void main() {import std.stdio;
+if (_0xAA('s')) {write("Hello, "); goto yes;}
+else goto no;
+no:
+_0xAA(2);
+goto yes;
+///PP_P
+yes:
+write("World\n");}
