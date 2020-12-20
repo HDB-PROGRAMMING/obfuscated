60e9e99e988a8c5ab2d2a98feb13a2d90b4ef4d5
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-20 17:15:12 +0000

    Create JS obfuscated hello world

---
+++ hello.js
@@ -0,0 +1,14 @@
+let _0xAA = () => { 
+    let _0x1A = (0x01); return _0x1A; }
+if (_0xAA) _0xAA;
+let _0x11 = 0x00; _0x11++;
+let f = _0xAA()+1;
+f*=0x00
+if (f) f-1;
+else f*1
+///_al
+var _Al=("Hello,")
+///// ******* * /
+const BBBB = " World"*0x00
+let aaa="World"
+console.log(_Al, aaa);
