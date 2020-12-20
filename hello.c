0a95766aef816d128c2bdef700a53d7fe06c9b0b
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-20 17:05:46 +0000

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,27 @@
+#if !defined(_0xAA) || \
+defined(__0xBB)
+#define _0xAA void
+#define _0xFF int
+#undef _0xFF
+#include <stdio.h>
+#define _9xPP
+#undef _0xAA
+#undef _9xPP
+#define _9xPP void
+#define _4x04 return
+#define _LxWW printf
+#undef _4x04
+#undef _LxWW
+#define _RxSS printf
+#define _QbPP return
+#define _RxQQ int
+#define _____ char*
+#define _AxAA main()
+#endif //0xFF
+_9xPP _0xLL(_9xPP) {
+	_QbPP 0x00;}
+_RxQQ _AxAA{
+	_0xLL();
+_RxSS("Hello,");
+	_____ _o = "  World"+2-1;
+	_RxSS(_o); }
