f34f5c5f93103516d585304e134fb0c88eb91b0c
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-20 17:26:42 +0000

    Create obfuscated python hello world

---
+++ py/hello.py
@@ -0,0 +1,24 @@
+_0x00=None
+#-*_aaaa
+def _LL(_9x77:_0x00)->_0x00:
+    return _0x00
+_AAAA=list(i for i in range(10))
+for i in _AAAA:
+    _LL(_0x00)
+    _LL(0x00)
+import math
+a = _LL
+a(_0x00)
+ff=a(_0x00)
+ff="Hello, "
+ll="0x00"
+__ll = a
+ll = ff != a(_0x00)
+if ll:
+    ll="World"
+else:
+    __ll(_0x00)
+if _0x00:
+    _LL(_0x00)
+else:
+    print(ff*1+ll*1)
